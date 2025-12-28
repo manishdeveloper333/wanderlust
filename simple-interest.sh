f034af2cc4fd8a88fdcd52d45849ba9bd381a3ec
Manish srivas <76111668+manishdeveloper333@users.noreply.github.com>
2025-12-28 14:49:20 +0530

    Add simple interest shell script

---
+++ simple-interest.sh
@@ -0,0 +1,15 @@
+# simple-interest.sh
+#!/bin/bash
+
+echo "Enter Principal amount:"
+read principal
+
+echo "Enter Rate of Interest:"
+read rate
+
+echo "Enter Time (in years):"
+read time
+
+simple_interest=$((principal * rate * time / 100))
+
+echo "Simple Interest is: $simple_interest"
